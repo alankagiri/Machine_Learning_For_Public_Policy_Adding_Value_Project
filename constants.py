8b3d09bd1eb717813cc5a79147ed4334e3dc8beb
alankagiri <alankagiri@uchicago.edu>
2025-04-16 12:23:05 -0400

    EDA, data cleaning, and early clustering steps so far

--- constants.py
+++ constants.py
@@ -3,4 +3,5 @@ columns_to_exclude = ['create_date_utc','closed_date_utc','last_action_utc', 'cr
     # The columns with '_utc' endings have the Eastern Time zone equivalent, and add little value to analysis, 
     # expecially in ML settings where such redudancies can lead to multicollinearity
 
-    # The 'cross_street' column has 90% of its values missing
+    # The 'cross_street' column has 90% of its values missing                 
+origin_dictionary = {'Call Center': 0, 'Website': 1, 'Control Panel': 2, 'Email': 3, 'Text Message': 4, 'Twitter': 5, 'Report2Gov Android': 6, 'Report2Gov Website': 7, 'Report2Gov iOS': 8}
